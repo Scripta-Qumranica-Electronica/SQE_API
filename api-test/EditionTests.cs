99f1ddbea3bf2df4041431af9f021799c3105d64
Bronson Brown-deVost <bronsonbdevost@aim.com>
2019-05-17 15:50:14 +0200

    All tests for edition controller finished

--- api-test/EditionTests.cs
+++ api-test/EditionTests.cs
@@ -1,61 +1,16 @@
-//using System.Net.Http;
-//using System.Threading.Tasks;
-//using Microsoft.AspNetCore.Mvc.Testing;
-//using SQE.SqeHttpApi.Server;
-//using Xunit;
-//
-//namespace ApiTests
-//{
-//    /// <summary>
-//    /// One instance of this will be created per test collection.
-//    /// </summary>
-//    public class CollectionFixture : ICollectionFixture<WebApplicationFactory<Startup>>
-//    {
-//        public readonly HttpClient Client;
-//
-//        public CollectionFixture()
-//        {
-//            var factory = new WebApplicationFactory<Startup>();
-//            Client = factory.CreateClient();
-//        }
-//    }
-//    
-//    [CollectionDefinition("Integration tests collection")]
-//    public class IntegrationTestsCollection : ICollectionFixture<CollectionFixture>
-//    {
-//        // This class has no code, and is never created. Its purpose is simply
-//        // to be the place to apply [CollectionDefinition] and all the
-//        // ICollectionFixture<> interfaces.
-//    }
-//   
-//    [Collection("Edition endpoint tests collection")]
-//    public class EditionTests
-//    {
-//        private readonly CollectionFixture _collectionFixture;
-//
-//        public EditionTests()
-//        {
-//            _collectionFixture = new CollectionFixture();
-//        }
-//
-//        [Fact]
-//        public async Task Test1()
-//        {
-//            // When
-//            //var response = await _collectionFixture.Client.GetStringAsync();
-//            Assert.Equal(1, 1);
-//        }
-//    }
-//}
-//
-
-using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using api_test.Helpers;
+using SQE.SqeHttpApi.Server.DTOs;
 using Xunit;
 
 namespace api_test
 {
+    /// <summary>
+    /// This test suite tests all the current endpoints in the EditionController
+    /// </summary>
     public class EditionTests
     {
         private readonly WebAppFactory _factory;
@@ -64,20 +19,209 @@ namespace api_test
             _factory = new WebAppFactory();
         }
 
-        [Theory]
-        [InlineData("/v1/editions")]
-        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        /// <summary>
+        /// Check if we can get editions when unauthenticated
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public async Task GetAllEditionsUnauthenticated()
         {
-
+            // ARRANGE
+            const string url = "/v1/editions";
+            
             // Act
-            var response = await _factory.Client.GetAsync(url);
+            var (response, msg) = await HttpRequest.SendAsync<string, EditionListDTO>(_factory.Client, HttpMethod.Get, url, null);
+            response.EnsureSuccessStatusCode();
 
             // Assert
-            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal("application/json; charset=utf-8",response.Content.Headers.ContentType.ToString());
+            Assert.True(msg.editions.Count > 0);
+        }
+        
+        /// <summary>
+        /// Check if we can get editions when unauthenticated
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public async Task GetOneEditionUnauthenticated()
+        {
+            // ARRANGE
+            const string url = "/v1/editions/1";
+            
+            // Act
+            var (response, msg) = await HttpRequest.SendAsync<string, EditionGroupDTO>(
+                _factory.Client, 
+                HttpMethod.Get, 
+                url, 
+                null);
+            response.EnsureSuccessStatusCode();
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal("application/json; charset=utf-8",response.Content.Headers.ContentType.ToString());
+            Assert.NotNull(msg.primary.name);
+        }
+        
+        /// <summary>
+        /// Check if we protect against disallowed unauthenticated requests
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public async Task RefuseUnauthenticatedEditionWrite()
+        {
+            // ARRANGE
+            const string url = "/v1/editions/1";
+            var payload = new EditionUpdateRequestDTO(){name = "none"};
+            
+            // Act (Create new scroll)
+            var (response, _) = await HttpRequest.SendAsync<EditionUpdateRequestDTO, EditionListDTO>(
+                _factory.Client, 
+                HttpMethod.Post, 
+                url, 
+                payload);
+            
+            // Assert
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+            
+            // Act (change scroll name)
+            (response, _) = await HttpRequest.SendAsync<EditionUpdateRequestDTO, EditionListDTO>(
+                _factory.Client, 
+                HttpMethod.Put, 
+                url, 
+                payload);
+            
+            // Assert
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+
+        /// <summary>
+        /// Test copying an edition
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public async Task CreateEdition()
+        {
+            // ARRANGE (with name)
+            const string url = "/v1/editions/1";
+            const string name = "interesting-long-test name @3#ח";
+            var newScrollRequest = new EditionUpdateRequestDTO() {name = name};
+            
+            //Act
+            var (response, msg) = await HttpRequest.SendAsync<EditionUpdateRequestDTO, EditionDTO>(
+                _factory.Client, 
+                HttpMethod.Post, 
+                url, 
+                newScrollRequest,
+                await HttpRequest.GetJWTAsync(_factory.Client));
+            response.EnsureSuccessStatusCode();
+
+            // Assert
             Assert.Equal("application/json; charset=utf-8",
                 response.Content.Headers.ContentType.ToString());
+            Assert.True(msg.name == name);
+            Assert.True(msg.id != 1);
             
-            var jwt = await Login.GetJWT(_factory.Client);
+            // ARRANGE (without name)
+            newScrollRequest = new EditionUpdateRequestDTO() {name = ""};
+            
+            //Act
+            (response, msg) = await HttpRequest.SendAsync<EditionUpdateRequestDTO, EditionDTO>(
+                _factory.Client, 
+                HttpMethod.Post, 
+                url, 
+                newScrollRequest,
+                await HttpRequest.GetJWTAsync(_factory.Client));
+            response.EnsureSuccessStatusCode();
+
+            // Assert
+            Assert.Equal("application/json; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+            Assert.True(msg.name != "");
+            Assert.True(msg.id != 1);
+        }
+        
+        /// <summary>
+        /// Test updating an edition
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public async Task UpdateEdition()
+        {
+            // ARRANGE
+            var bearerToken = await HttpRequest.GetJWTAsync(_factory.Client);
+            var editionId = await HttpRequest.CreateNewEdition(_factory.Client);
+            var url = "/v1/editions/" + editionId;
+            var (response, msg) = await HttpRequest.SendAsync<string, EditionGroupDTO>(
+                _factory.Client, 
+                HttpMethod.Get, 
+                url, 
+                null,
+                bearerToken);
+            response.EnsureSuccessStatusCode();
+            var oldName = msg.primary.name;
+            const string name = "מגלה א";
+            var newScrollRequest = new EditionUpdateRequestDTO {name = name};
+            
+            //Act
+            var (response2, msg2) = await HttpRequest.SendAsync<EditionUpdateRequestDTO, EditionDTO>(
+                _factory.Client, 
+                HttpMethod.Put, 
+                url, 
+                newScrollRequest,
+                bearerToken);
+            response2.EnsureSuccessStatusCode();
+
+            // Assert
+            Assert.Equal("application/json; charset=utf-8",
+                response2.Content.Headers.ContentType.ToString());
+            Assert.True(msg2.name != oldName);
+            Assert.True(msg2.name == name);
+            Assert.True(msg2.id == editionId);
+        }
+        
+        /// <summary>
+        /// Check that we get private editions when authenticated, and don't get them when unauthenticated.
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public async Task GetPrivateEditions()
+        {
+            // ARRANGE
+            var bearerToken = await HttpRequest.GetJWTAsync(_factory.Client);
+            var editionId = await HttpRequest.CreateNewEdition(_factory.Client);
+            const string url = "/v1/editions";
+            
+            // Act (get listings with authentication)
+            var (response, msg) = await HttpRequest.SendAsync<string, EditionListDTO>(
+                _factory.Client, 
+                HttpMethod.Get, 
+                url, 
+                null, 
+                bearerToken);
+            response.EnsureSuccessStatusCode();
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal("application/json; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+            Assert.True(msg.editions.Count > 0);
+            Assert.Contains(msg.editions.SelectMany(x => x), x => x.id == editionId);
+            
+            // Act (get listings without authentication)
+            (response, msg) = await HttpRequest.SendAsync<string, EditionListDTO>(
+                _factory.Client, 
+                HttpMethod.Get, 
+                url, 
+                null);
+            response.EnsureSuccessStatusCode();
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal("application/json; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+            Assert.True(msg.editions.Count > 0);
+            Assert.DoesNotContain(msg.editions.SelectMany(x => x), x => x.id == editionId);
         }
     }
 }
---
+++ api-test/Helpers/HttpRequest.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using SQE.SqeHttpApi.Server.DTOs;
+
+namespace api_test.Helpers
+{ 
+    public static class HttpRequest
+    {
+        /// <summary>
+        /// Wrapper to make HTTP requests (with authorization) and return the response with the required class.
+        /// </summary>
+        /// <param name="client">The HttpClient to make the request</param>
+        /// <param name="httpMethod">The type of requests: GET/POST/PUT/DELETE</param>
+        /// <param name="url">The requested url (should start with a /), the SQE_API address is automatically prepended</param>
+        /// <param name="bearer">The current bearer token of the requesting client.</param>
+        /// <param name="payload">Optional class T1 to be sent as a stringified JSON object.</param>
+        /// <returns>Returns an HttpStatusCode for the request and a parsed object T2 with the response.</returns>
+        public static async Task<(HttpResponseMessage response, T2 msg)> SendAsync<T1,T2>(
+            HttpClient client,
+            HttpMethod httpMethod, 
+            string url,
+            T1 payload,
+            string bearer = null)
+        {
+            // Initialize the response
+            var parsedClass = default(T2);
+            var response = new HttpResponseMessage();
+            
+            // Create the request message.  Automatically disposed after the using block ends.
+            using(var requestMessage = new HttpRequestMessage(httpMethod, url))
+            {
+                try
+                {
+                    StringContent jsonPayload = null;
+                    if (!string.IsNullOrEmpty(bearer)) // Add the bearer token
+                        requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", bearer);
+                    
+                    if (payload != null) // Add payload if it exists.
+                    {
+                        var json = JsonConvert.SerializeObject(payload);
+                        jsonPayload = new StringContent(json, UnicodeEncoding.UTF8, "application/json");
+                        requestMessage.Content = jsonPayload;
+                    }
+                    
+                    // Make the request and capture the response and http status message.
+                    response = await client.SendAsync(requestMessage);
+                    var responseBody = await response.Content.ReadAsStringAsync();
+                    parsedClass = JsonConvert.DeserializeObject<T2>(responseBody);
+                }  
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+            return (response: response, msg: parsedClass);
+        }
+        
+        public static async Task<string> GetJWTAsync(HttpClient client)
+        {
+            const string name = "test";
+            const string password = "asdf";
+            var login = new LoginRequestDTO(){ userName = name, password = password};
+            var (response, msg) = await SendAsync<LoginRequestDTO, LoginResponseDTO>(client, HttpMethod.Post, "/v1/users/login", login);
+            response.EnsureSuccessStatusCode();
+            return msg.token;
+        }
+
+        public static async Task<uint> CreateNewEdition(HttpClient client)
+        {
+            var newScrollRequest = new EditionUpdateRequestDTO() {name = "test-name"};
+            var (response, msg) = await SendAsync<EditionUpdateRequestDTO, EditionDTO>(client, HttpMethod.Post, "/v1/editions/1", newScrollRequest, await GetJWTAsync(client));
+            response.EnsureSuccessStatusCode();
+            return msg.id;
+        }
+    }
+}
