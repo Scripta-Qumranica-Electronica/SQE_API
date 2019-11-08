4ca338b7be85abddfb14e67b4a0a113de3daebea
Bronson Brown-deVost <bronsonbdevost@aim.com>
2019-11-08 15:41:06 +0100

    fixed query

--- sqe-database-access/Queries/Artefact.cs
+++ sqe-database-access/Queries/Artefact.cs
@@ -29,25 +29,25 @@ SELECT artefact_data.name AS Name,
        image_catalog.catalog_side AS CatalogSide, 
        SQE_image.image_catalog_id AS ImageCatalogId,
        work_status.work_status_message AS WorkStatusMessage
-FROM edition
+FROM artefact_shape_owner
+    JOIN edition USING(edition_id)
 	JOIN edition_editor USING(edition_id)
-	JOIN artefact_shape_owner USING(edition_id)
 	JOIN artefact_shape USING(artefact_shape_id)
-	LEFT JOIN artefact_position_owner ON artefact_position_owner.edition_id = @EditionId
+	
 	LEFT JOIN artefact_position ON artefact_position.artefact_id = artefact_shape.artefact_id
-	    AND artefact_position.artefact_position_id = artefact_position_owner.artefact_position_id
-	LEFT JOIN artefact_status_owner ON artefact_status_owner.edition_id = @EditionId
-    LEFT JOIN artefact_status ON artefact_status_owner.artefact_status_id = artefact_status.artefact_status_id
-    	AND artefact_shape.artefact_id = artefact_status.artefact_id
-    LEFT JOIN work_status ON artefact_status.work_status_id = work_status.work_status_id
+	LEFT JOIN artefact_position_owner ON artefact_position_owner.artefact_position_id =  artefact_position.artefact_position_id AND artefact_position_owner.edition_id = 1646
+	
+	LEFT JOIN artefact_status ON artefact_status.artefact_id = artefact_shape.artefact_id
+	LEFT JOIN artefact_status_owner ON artefact_status_owner.artefact_status_id = artefact_status.artefact_status_id AND artefact_status_owner.edition_id = 1646
+	
 	JOIN artefact_data ON artefact_data.artefact_id = artefact_shape.artefact_id
 	JOIN artefact_data_owner ON artefact_data.artefact_data_id = artefact_data_owner.artefact_data_id
 	    AND edition.edition_id = artefact_data_owner.edition_id
 	
-	JOIN SQE_image USING(sqe_image_id)
+	LEFT JOIN SQE_image USING(sqe_image_id)
 	JOIN image_catalog USING(image_catalog_id)
 
-WHERE edition.edition_id = @EditionId
+WHERE artefact_shape_owner.edition_id = @EditionId
   AND $Restriction
 $Order";
 
