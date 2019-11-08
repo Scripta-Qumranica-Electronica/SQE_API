b1d3dc6fe481858bc194758711c93a7db196de7c
Bronson Brown-deVost <bronsonbdevost@aim.com>
2019-11-08 18:40:51 +0100

    Artefact search bugfix

--- sqe-database-access/Queries/Artefact.cs
+++ sqe-database-access/Queries/Artefact.cs
@@ -35,10 +35,10 @@ FROM artefact_shape_owner
 	JOIN artefact_shape USING(artefact_shape_id)
 	
 	LEFT JOIN artefact_position ON artefact_position.artefact_id = artefact_shape.artefact_id
-	LEFT JOIN artefact_position_owner ON artefact_position_owner.artefact_position_id =  artefact_position.artefact_position_id AND artefact_position_owner.edition_id = 1646
+	LEFT JOIN artefact_position_owner ON artefact_position_owner.artefact_position_id =  artefact_position.artefact_position_id AND artefact_position_owner.edition_id = @EditionId
 	
 	LEFT JOIN artefact_status ON artefact_status.artefact_id = artefact_shape.artefact_id
-	LEFT JOIN artefact_status_owner ON artefact_status_owner.artefact_status_id = artefact_status.artefact_status_id AND artefact_status_owner.edition_id = 1646
+	LEFT JOIN artefact_status_owner ON artefact_status_owner.artefact_status_id = artefact_status.artefact_status_id AND artefact_status_owner.edition_id = @EditionId
 	LEFT JOIN work_status USING(work_status_id)
 	
 	JOIN artefact_data ON artefact_data.artefact_id = artefact_shape.artefact_id
