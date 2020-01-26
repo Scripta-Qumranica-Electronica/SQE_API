1dccf0118e398028442744b3a3f23acc4e98860e
Itay Zandbank <itay@chelem.co.il>
2020-01-26 14:10:04 +0200

    Add the hub events interface

---
+++ sqe-api-server/HubEvents.cs
@@ -0,0 +1,24 @@
+﻿using SQE.API.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQE.API.Server.RealtimeHubs
+{
+    interface IHubEvents
+    {
+        Task UpdatedArtefact(ArtefactDTO artefact);
+        Task CreatedArtefact(ArtefactDTO artefact);
+        Task DeletedArtefact(DeleteEditionEntityDTO deletedEntity);
+
+        Task UpdatedEdition(EditionDTO edition);
+        Task DeletedEdition(DeleteEditionEntityDTO deletedntity);
+        Task AddedEditionEditor(EditorRightsDTO editor);
+        Task UpdatedEditionEditor(EditorRightsDTO editor);
+
+        Task CreatedROIs(InterpretationRoiDTOList rois);
+        Task UpdatedROIs(UpdatedInterpretationRoiDTOList rois);
+        Task DeletedROIs(List<uint> rois);
+    }
+}
