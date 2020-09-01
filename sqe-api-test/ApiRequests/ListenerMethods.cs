3b46ae5b286bcafef2413aedae1e574cf1a96c4f
Bronson Brown-deVost <bronsonbdevost@aim.com>
2020-09-01 17:58:00 +0200

    cleared warnings and errors

--- sqe-api-test/ApiRequests/ListenerMethods.cs
+++ sqe-api-test/ApiRequests/ListenerMethods.cs
@@ -28,6 +28,6 @@ namespace SQE.ApiTest.ApiRequests
         DeletedAttribute,
         DeletedSignInterpretation,
         UpdatedAttribute,
-        CreatedTextFragment
+        CreatedTextFragment,
     }
 }
